aa8ce2727f8e492b296ac62cbdb434b38c48c278
OlehDidushok <didushokov@gmail.com>
2024-01-07 21:52:56 +0200

    Added MotionAnimationView

--- HikeApp/Component/CustomCircleView.swift
+++ HikeApp/Component/CustomCircleView.swift
@@ -11,23 +11,25 @@ struct CustomCircleView: View {
     @State private var isAnimateGradient: Bool = false
     
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(colors: [
-                    .customIndigoMedium,
-                    .customSaimonLight
-                ],
-                               startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
-                               endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
-                              )
-            )
-            .onAppear {
-                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
-                    isAnimateGradient.toggle()
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors: [
+                        .customIndigoMedium,
+                        .customSaimonLight
+                    ],
+                                   startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                                   endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                                  )
+                )
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimateGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256, height: 256)
-        
+       MotionAnimationView()
+        }
+        .frame(width: 256, height: 256)
 
     }
 }
